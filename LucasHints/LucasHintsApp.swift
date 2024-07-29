01215655e8326d3e3f606e8ae33f3bd6e628c1b8
aaron <alkrauss48@gmail.com>
2024-07-28 20:02:03 -0500

    Initial Commit

---
+++ LucasHints/LucasHintsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LucasHintsApp.swift
+//  LucasHints
+//
+//  Created by Aaron Krauss on 7/28/24.
+//
+
+import SwiftUI
+
+@main
+struct LucasHintsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
