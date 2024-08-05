c2293d1651695b419bcca6e88d6f8050cfba5e6b
aaron <alkrauss48@gmail.com>
2024-08-05 15:09:46 -0500

    test: add tests for app state processes

--- LucasHintsTests/LucasHintsTests.swift
+++ LucasHintsTests/LucasHintsTests.swift
@@ -9,29 +9,30 @@ import XCTest
 @testable import LucasHints
 
 final class LucasHintsTests: XCTestCase {
+    var appState: AppState?
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        appState = AppState()
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testAppStateInit() throws {
+        XCTAssert(appState!.activeGlyphs.count > 0)
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete.
-        // Check the results with assertions afterwards.
+    func testAppStateRegenerate() throws {
+        let originalGlyphs = Set(appState!.activeGlyphs)
+
+        appState!.regenerateUiGlyphs()
+
+        let newGlyphs = Set(appState!.activeGlyphs)
+
+        XCTAssert(newGlyphs.intersection(originalGlyphs).count == 0)
     }
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         self.measure {
-            // Put the code you want to measure the time of here.
+            appState!.regenerateUiGlyphs()
         }
     }
-
 }
