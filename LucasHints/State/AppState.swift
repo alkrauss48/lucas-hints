9f9d9b1b05a3f961f4be87d456717ed7c38e056b
aaron <alkrauss48@gmail.com>
2024-08-05 10:42:34 -0500

    feat: ensure new glyphs will have no overlap of previous glyphs

--- LucasHints/State/AppState.swift
+++ LucasHints/State/AppState.swift
@@ -18,10 +18,25 @@ class AppState: ObservableObject {
     }
 
     func regenerateUiGlyphs() {
+        // Create a set of the original active glyph asset names.
+        let oldGlyphAssetNames = Set(self.activeGlyphs.map { $0.glyph.assetName })
+
+        // Generate new active glyphs.
         self.activeGlyphs = categories
             .shuffled()
             .prefix(3)
             .map { UIGlyph(glyph: $0.glyphs.randomElement()!, category: $0)}
+
+        // Verify that the new glyphs are all different than the original ones.
+        let notAllNewGlyphs = Set(self.activeGlyphs.map { $0.glyph.assetName })
+            .intersection(oldGlyphAssetNames)
+            .count > 0
+
+        // If there is any overlap in old-to-new glyphs,
+        // then regenerate the glyphs again.
+        if notAllNewGlyphs {
+            regenerateUiGlyphs()
+        }
     }
 }
 
