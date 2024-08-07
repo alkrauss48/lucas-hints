003b2935c756bf2e2f44ff146651479c578bd562
aaron <alkrauss48@gmail.com>
2024-08-07 10:26:28 -0500

    chore: set iPad navigation style to stack style

--- LucasHints/ContentView.swift
+++ LucasHints/ContentView.swift
@@ -37,6 +37,7 @@ struct ContentView: View {
                 }
             }
         }
+        .navigationViewStyle(StackNavigationViewStyle())
     }
 
     func verticalDisplay(width: CGFloat) -> some View {
